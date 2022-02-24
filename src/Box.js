a751263359fe7df2d561ff8b0344d80c483afe87
Gianmarco G <giangarof98@gmail.com>
2022-02-24 16:53:27 -0500

    colorbox

---
+++ src/Box.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import { choice } from "./helpers";
+import './Box.css'
+
+class Box extends Component{
+    // static defaultProps ={
+    //     allColors: ['magenta', 'aqua','cadetblue', 'lilac', 'royalblue']
+
+    // }
+    constructor(props){
+        super(props);
+        this.state = {color: choice(this.props.colors)}
+        this.handleClick= this.handleClick.bind(this);
+    }
+    pickColor(){
+        let newColor; 
+        do {
+            newColor = choice(this.props.colors);
+        } while (newColor === this.state.color)
+        this.setState({color:newColor})
+    }
+    handleClick(){
+        this.pickColor()
+    }
+    render(){
+        return(
+            <div className="Box" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
+
+            </div>
+        )
+    }
+}
+
+export default Box
